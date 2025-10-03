df329268830e17133dd400b25a3b54e02849e963
arpinedilanyan <arpinee18@gmail.com>
2025-10-03 13:49:17 +0400

    New project

---
+++ lab01.js
@@ -0,0 +1,22 @@
+let user1 = { name: 'Arpi', age21, isAdmin: false };
+let  x = "name";
+console.log(user1.name);
+console.log(user1['name']);
+console.log(user1.[x]);
+
+function User(name, age){
+    this.name= name;
+    this.age = age;
+}
+
+let urer1 = new User('Arpi', 21);
+let user2 = new User('Monika', 5);
+console.log(urer1);
+
+let  date1 = new Date();
+
+
+class User{
+
+
+}
